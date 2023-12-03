205e3fd37f862941a0587a05e1adfa49e1d4e57c
“Saphron” <109723086+denerose@users.noreply.github.com>
2023-12-04 00:00:17 +1100

    Day 3 - Part 1

--- day3/part1.test.ts
+++ day3/part1.test.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import { part1, part2 } from "./day3";
+import { part1 } from "./part1";
 
 describe("part1 example", () => {
     const input = [
@@ -18,19 +18,13 @@ describe("part1 example", () => {
     test("should return correct result for example data", () => {
         expect(part1(input)).toBe(expectedResult);
     });
-    test("should return correct result for example data with extra line on end", () => {
-        expect(part1([...input, ""])).toBe(expectedResult);
-    });
 });
 
-// describe("part1 full", () => {
-//   const input = readFileSync("./src/day3/input.txt").toString().split("\n");
-//   const result = 0;
+describe("part1 full", () => {
+    const input = readFileSync("./day3/data.txt").toString().split("\n");
+    const result = 532331;
 
-//   test("should return correct result for real data", () => {
-//     expect(part1(input)).toBe(result);
-//   });
-//   test("should return correct result for example data with extra line on end", () => {
-//     expect(part1([...input, ""])).toBe(result);
-//   });
-// });
+    test("should return correct result for real data", () => {
+        expect(part1(input)).toBe(result);
+    });
+});
--- day3/part1.ts
+++ day3/part1.ts
@@ -1,24 +1,97 @@
 import { readFileSync } from 'fs'
 
-const symbolsRegEx = /[^.|^\d|^\s]/g
+const symbolsRegEx = /[^.|^\d|^\s]/
+const numsRegEx = /\d/
+const findNumRegEx = /\d{1,3}/g
+
+const partNumbers: number[] = []
 
 function fetchData() {
-    let rawGames: string[] = []
-    rawGames = readFileSync('day3/data.txt').toString().split('\n')
-    return rawGames
+    let rawData: string[] = []
+    rawData = readFileSync('day3/data.txt').toString().split('\n')
+    // let mappedData = create2dArray(rawData)
+    return rawData
 }
 
-function findLinkedNums(input: string) {
-
-}
-
-function part1(data: string[]) {
-    data.forEach((rowString) => {
-        const y = data.indexOf(rowString)
-        const row = Array.from(rowString)
-        row.forEach((char) => {
-            symbolsRegEx.test(char)
-        })
-
+function create2dArray(rawData: string[]) {
+    const mappedArray: string[][] = []
+    rawData.forEach((rowString) => {
+        mappedArray.push(Array.from(rowString))
     })
+    return mappedArray
 }
+
+function findLinkedNums(data: string[], x: number, y: number) {
+    const rowsToSearch = [y - 1, y, y + 1]
+    rowsToSearch.forEach((row) => {
+        let xStart: number = x - 1
+        let xEnd: number = x + 1
+        if (
+            !numsRegEx.test(data[row][x - 1])
+            && !numsRegEx.test(data[row][x])
+            && !numsRegEx.test(data[row][x + 1])) {
+            return
+        }
+        else {
+            if (numsRegEx.test(data[row][x - 1])) {
+                if (numsRegEx.test(data[row][x - 3]) && numsRegEx.test(data[row][x - 2])) {
+                    xStart = x - 3
+                }
+                else if (numsRegEx.test(data[row][x - 2])) {
+                    xStart = x - 2
+                }
+                else xStart = x - 1
+            }
+            else {
+                xStart = x - 1
+            }
+
+            if (numsRegEx.test(data[row][x + 1])) {
+                if (!numsRegEx.test(data[row][x + 2])) {
+                    xEnd = x + 1
+                }
+                else if (!numsRegEx.test(data[row][x + 3])) {
+                    xEnd = x + 2
+                }
+                else if (numsRegEx.test(data[row][x + 3])) {
+                    xEnd = x + 3
+                }
+            }
+            else { xEnd = x + 1 }
+        }
+        const stringToCheck = data[row].substring(xStart, xEnd + 1)
+        const partsFound = stringToCheck.match(findNumRegEx)
+        if (partsFound) partsFound.forEach((part) => {
+            partNumbers.push(Number(part))
+        })
+    })
+
+}
+
+function sum(total: number, num: number) {
+    return total + num
+}
+
+export function part1(data: string[]) {
+    data.forEach((row) => {
+        let i = 0
+        const y = data.indexOf(row)
+        const rowArray = Array.from(row)
+        rowArray.forEach((char) => {
+            if (!symbolsRegEx.test(char)) {
+                i++
+                return
+            }
+            else {
+                const x = i
+                findLinkedNums(data, x, y)
+                i++
+            }
+        })
+    })
+    const result = partNumbers.reduce(sum)
+    console.log(result)
+    return result
+}
+
+part1(fetchData())
