7175b813858193f6fcf7156c98ef18636a307e24
“Saphron” <109723086+denerose@users.noreply.github.com>
2023-12-05 22:05:48 +1100

    Day 4 - Part 2

--- day4/day4.ts
+++ day4/day4.ts
@@ -10,11 +10,32 @@ function sum(total: number, num: number) {
     return total + num
 }
 
-function splitCards(input: string) {
+// function splitCards(input: string) {
+//     const splitString = input.split(/\:|\|/g)
+//     const cardNums = splitString[1].match(/\d{1,2}/g)
+//     const winNums = splitString[2].match(/\d{1,2}/g)
+//     return { cardNums, winNums }
+// }
+
+interface gameCard {
+    cardID: string,
+    cardNums: RegExpMatchArray | null,
+    winNums: RegExpMatchArray | null,
+    winCount: number
+}
+
+function splitCards(input: string): gameCard {
     const splitString = input.split(/\:|\|/g)
+    const cardID = splitString[0]
     const cardNums = splitString[1].match(/\d{1,2}/g)
     const winNums = splitString[2].match(/\d{1,2}/g)
-    return { cardNums, winNums }
+    let winCount: number = 0
+    cardNums?.forEach((num) => {
+        if (winNums?.includes(num)) {
+            winCount++
+        }
+    })
+    return { cardID, cardNums, winNums, winCount }
 }
 
 function findScore(n: number) {
@@ -45,12 +66,37 @@ export function part1(data: string[]) {
     })
 
     const result = cardScores.reduce(sum)
-    console.log(`final result: ${result}`)
+    console.log(`part1: final result = ${result}`)
     return result
 }
 
 part1(fetchData())
 
+function createStack(data: string[]) {
+    const newStack: gameCard[] = []
+    data.forEach((cardString) => { newStack.push(splitCards(cardString)) })
+    return newStack
+}
+
+function countWins(data: gameCard[], n: number) {
+    let count = data[n].winCount
+    let wins = 1
+    for (let i = count; i > 0; i--) {
+        wins += countWins(data, n + i)
+    }
+    return wins
+}
+
 export function part2(data: string[]) {
+    const initialStack = createStack(data)
+    let allWins = 0
+    for (let index = 0; index < data.length; index++) {
+        allWins += countWins(initialStack, index)
+
+    }
+    console.log(allWins)
+    return allWins
 
 }
+
+part2(fetchData())
